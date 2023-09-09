3bf71f7a428b03c08f99096de3dec624016f410a
zansy@outlook.com <zansy@outlook.com>
2023-09-09 14:20:25 +0800

    added mia folder

---
+++ mia/miaApp.swift
@@ -0,0 +1,17 @@
+//
+//  miaApp.swift
+//  mia
+//
+//  Created by Zansy Lee on 2023/8/22.
+//
+
+import SwiftUI
+
+@main
+struct miaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
